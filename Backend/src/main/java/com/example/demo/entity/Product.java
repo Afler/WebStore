b0ae8ba7a6cb272816ced45a0c417ffb24b0889b
Lev <afler777@gmail.com>
2022-02-25 17:18:07 +0400

    Missing file was added

--- Backend/src/main/java/com/example/demo/entity/Product.java
+++ Backend/src/main/java/com/example/demo/entity/Product.java
@@ -1,16 +1,20 @@
 package com.example.demo.entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 
 @Entity
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Product {
 
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
-    private long id;
+    private Long id;
     private String name;
     private double price;
     private int quantity;
