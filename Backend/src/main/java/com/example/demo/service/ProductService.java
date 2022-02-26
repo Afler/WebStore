783340ca2d224f727e8a41205a5c0d1150c6c8fa
Андрей Крупников <cjrrf@mail.ru>
2022-02-26 08:58:30 +0400

    Исправил файлы

--- Backend/src/main/java/com/example/demo/service/ProductService.java
+++ Backend/src/main/java/com/example/demo/service/ProductService.java
@@ -1,23 +1,10 @@
 package com.example.demo.service;
 
 import com.example.demo.entity.Product;
-import com.example.demo.repository.ProductRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-@Service
-public class ProductService {
+public interface ProductService {
 
-    private final ProductRepository productRepository;
-
-    @Autowired
-    public ProductService(ProductRepository productRepository) {
-        this.productRepository = productRepository;
-    }
-
-    public List<Product> getProductsByCategory(String category) {
-        return productRepository.findByCategory(category);
-    }
+    List<Product> getProductsByCategory(String category);
 }
---
+++ Backend/src/main/java/com/example/demo/serviceImpl/ProductServiceImpl.java
@@ -0,0 +1,21 @@
+package com.example.demo.serviceImpl;
+
+import com.example.demo.entity.Product;
+import com.example.demo.repository.ProductRepository;
+import com.example.demo.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductServiceImpl implements ProductService {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Override
+    public List<Product> getProductsByCategory(String category) {
+        return productRepository.findByCategory(category);
+    }
+}
