30560e17a8b1b092035319d30480aaa08be289e2
Андрей Крупников <cjrrf@mail.ru>
2022-02-26 21:08:24 +0400

    Сделал инжект через конструктор

--- Backend/src/main/java/com/example/demo/serviceImpl/UserServiceImpl.java
+++ Backend/src/main/java/com/example/demo/serviceImpl/UserServiceImpl.java
@@ -3,14 +3,16 @@ package com.example.demo.serviceImpl;
 import com.example.demo.entity.User;
 import com.example.demo.repository.UserRepository;
 import com.example.demo.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserServiceImpl implements UserService {
 
-    @Autowired
-    private UserRepository userRepository;
+    private final UserRepository userRepository;
+
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
     @Override
     public User saveUser(User user) {
