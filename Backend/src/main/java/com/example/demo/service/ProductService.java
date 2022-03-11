4b308733a7897725ba6e0e6d9bdd00dbd82716b5
Lev <afler777@gmail.com>
2022-03-11 19:52:39 +0400

    naming fixed again

--- Backend/src/main/java/com/example/demo/service/ProductService.java
+++ Backend/src/main/java/com/example/demo/service/ProductService.java
@@ -2,11 +2,7 @@ package com.example.demo.Service;
 
 import com.example.demo.entity.Product;
 
-import java.util.List;
-
 public interface ProductService {
-
-    List<Product> getProductsByCategory(String category);
 
     Product saveProduct(Product product);
 
