f2ed1aa442eee926c49c667afaf7d2528626337c
Lev <afler777@gmail.com>
2022-03-11 19:21:14 +0400

    package naming fixed

--- Backend/src/main/java/com/example/demo/service/ProductService.java
+++ Backend/src/main/java/com/example/demo/service/ProductService.java
@@ -2,9 +2,10 @@ package com.example.demo.service;
 
 import com.example.demo.entity.Product;
 
-import java.util.List;
-
 public interface ProductService {
 
-    List<Product> getProductsByCategory(String category);
+    Product saveProduct(Product product);
+
+    Product createNewProduct(String name, double cost, int quantity, String image);
+
 }
--- Backend/src/main/java/com/example/demo/serviceImpl/ProductServiceImpl.java
+++ Backend/src/main/java/com/example/demo/serviceImpl/ProductServiceImpl.java
@@ -1,21 +1,32 @@
 package com.example.demo.serviceImpl;
 
+import com.example.demo.service.ProductService;
 import com.example.demo.entity.Product;
 import com.example.demo.repository.ProductRepository;
-import com.example.demo.service.ProductService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
 
 @Service
 public class ProductServiceImpl implements ProductService {
 
-    @Autowired
-    private ProductRepository productRepository;
+    private final ProductRepository productRepository;
+
+    public ProductServiceImpl(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
 
     @Override
-    public List<Product> getProductsByCategory(String category) {
-        return productRepository.findByCategory(category);
+    public Product saveProduct(Product product) {
+        return productRepository.save(product);
+    }
+
+    @Override
+    public Product createNewProduct(String name, double cost, int quantity, String image) {
+        Product product = new Product();
+        product.setName(name);
+        product.setCost(cost);
+        product.setQuantity(quantity);
+        product.setImage(image);
+        return product;
     }
 }
