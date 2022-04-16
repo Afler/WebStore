373d860e9bac646dc7471c9a9dbbbbc96a36ac43
Андрей Крупников <cjrrf@mail.ru>
2022-04-16 11:18:34 +0400

    добавил прямую и обратную сортировку по имени и по количеству и прямую сортировку по цене

--- Backend/src/main/java/com/example/demo/serviceImpl/ProductServiceImpl.java
+++ Backend/src/main/java/com/example/demo/serviceImpl/ProductServiceImpl.java
@@ -56,7 +56,32 @@ public class ProductServiceImpl implements ProductService {
     }
 
     @Override
-    public List<Product> sortProductsByPrice(List<Product> products) {
+    public List<Product> sortProductsByPriceAsc(List<Product> products) {
+        return productRepository.findAll(Sort.by(Sort.Direction.ASC, "cost"));
+    }
+
+    @Override
+    public List<Product> sortProductsByPriceDesc(List<Product> products) {
         return productRepository.findAll(Sort.by(Sort.Direction.DESC, "cost"));
+    }
+
+    @Override
+    public List<Product> sortProductsByNameAsc(List<Product> products) {
+        return productRepository.findAll(Sort.by(Sort.Direction.ASC, "name"));
+    }
+
+    @Override
+    public List<Product> sortProductsByNameDesc(List<Product> products) {
+        return productRepository.findAll(Sort.by(Sort.Direction.DESC, "name"));
+    }
+
+    @Override
+    public List<Product> sortProductsByCountAsc(List<Product> products) {
+        return productRepository.findAll(Sort.by(Sort.Direction.ASC, "quantity"));
+    }
+
+    @Override
+    public List<Product> sortProductsByCountDesc(List<Product> products) {
+        return productRepository.findAll(Sort.by(Sort.Direction.DESC, "quantity"));
     }
 }
